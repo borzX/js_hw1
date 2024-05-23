67ad29e643e9970942b93d00d3f9677dddd7a70f
borz <117116989+borzX@users.noreply.github.com>
2024-05-23 10:09:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/* +++ Задание 1 +++ */
+
+let temperatureCelsium = 10
+let temperatureFaregheit = (9/5)*temperatureCelsium+32
+
+console.log(`Температура по Цельсию: \t${temperatureCelsium}`)
+console.log(`Температура по Фаренгейту: \t${temperatureFaregheit}`);
+
+console.log(` \n `)
+
+
+/* +++ Задание 2 +++ */
+
+const name = 'Дмитрий'
+let admin = name
+
+console.log(`admin: ${admin}`)
